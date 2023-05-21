d2ee537001ebde66eb5f7e459deb5f0e6f76c56c
kobkumar-nd-wahbe <kobkumar@deloitte.com>
2023-05-21 15:35:48 +0530

    second commit

---
+++ functions/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const mysql = require('mysql');
+const cors = require('cors');
+const router = express.Router();
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host:'sql12.freesqldatabase.com',
+    user:'sql12619557',
+    password:'bUTKs4fjLk',
+    database:'sql12619557'
+});
+
+app.get('/',(re, res)=>{
+    return res.json("From Backend side");
+});
+
+app.get('/employees',(req, res)=>{
+    const sql = "select * from Employee";
+    db.query(sql,(err, data)=>{
+        if(err) return res.json(err);
+        else return res.json(data);
+    })
+});
+
+
+app.listen(8081, () =>{
+    console.log("listening");
+})
+app.use('./netlify/functions/server',router);
+module.exports.handler = serverless(app);
